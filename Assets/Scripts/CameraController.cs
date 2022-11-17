1c5de196d8ae301afd674099be5c96e4ece4726c
Alexandru-Sergiu Marton <brown121407@posteo.ro>
2022-11-17 14:15:24 +0200

    Don't let the camera see outside the game field.

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -1,18 +1,55 @@
+using Constants;
 using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    [SerializeField] private Transform target;
-    [SerializeField] private Vector3 tOffset;
-    [SerializeField] private float cSpeed;
+    [SerializeField] private float smoothSpeed = 3.5f;
 
-    private void Update()
+    private Camera _camera;
+    private Transform _playerTransform;
+    private Vector3 _offset;
+
+    private void Awake()
     {
-        MoveCamera();
+        _camera = GetComponent<Camera>();
+
+        _playerTransform = GameObject.FindWithTag(Tags.Player).transform;
+        _offset = transform.position - _playerTransform.position;
     }
 
-    private void MoveCamera()
+    private void LateUpdate()
     {
-        transform.position = Vector3.Lerp(transform.position, target.position + tOffset, cSpeed * Time.deltaTime);
+        var newCameraPos = _playerTransform.position + _offset;
+
+        // Check if the edges of the camera are seeing outside of the floor plane.
+        // Left
+        var ray = _camera.ViewportPointToRay(new Vector3(0.0f, 0.5f, 0.0f));
+        if (!Physics.Raycast(ray, Mathf.Infinity, (int) Layers.Floor))
+        {
+            newCameraPos.x = Mathf.Max(transform.position.x, newCameraPos.x);
+        }
+
+        // Right
+        ray = _camera.ViewportPointToRay(new Vector3(1.0f, 0.5f, 0.0f));
+        if (!Physics.Raycast(ray, Mathf.Infinity, (int) Layers.Floor))
+        {
+            newCameraPos.x = Mathf.Min(transform.position.x, newCameraPos.x);
+        }
+
+        // Bottom
+        ray = _camera.ViewportPointToRay(new Vector3(0.5f, 0.0f, 0.0f));
+        if (!Physics.Raycast(ray, Mathf.Infinity, (int) Layers.Floor))
+        {
+            newCameraPos.z = Mathf.Max(transform.position.z, newCameraPos.z);
+        }
+
+        // Top
+        ray = _camera.ViewportPointToRay(new Vector3(0.5f, 1.0f, 0.0f));
+        if (!Physics.Raycast(ray, Mathf.Infinity, (int) Layers.Floor))
+        {
+            newCameraPos.z = Mathf.Min(transform.position.z, newCameraPos.z);
+        }
+
+        transform.position = Vector3.Lerp(transform.position, newCameraPos, smoothSpeed * Time.deltaTime);
     }
 }
---
+++ Assets/Scripts/Constants/Layers.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace Constants
+{
+    [Flags]
+    public enum Layers
+    {
+        Floor = 1 << 7
+    }
+}
